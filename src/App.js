55cd4cf1904e7351e75d6bcea897e2c70b5ca0e4
Moisés Ferreira <moises.ferreira@ctw.bmwgroup.com>
2021-08-16 12:39:10 +0100

    First draft

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bem vindo ao branch do Maf.
+          Bem vindo ao site AngoDenúncia! Ainda em construção...
         </p>
         <a
           className="App-link"
